f64aa2d0b5fb5d605038bcd57befc2cc114484ef
pifeng <709343767@qq.com>
2020-03-25 09:26:21 +0800

    去除空包

--- monitoring-server-web/src/main/java/com/transfar/server/business/server/controller/ServerController.java
+++ monitoring-server-web/src/main/java/com/transfar/server/business/server/controller/ServerController.java
@@ -1,18 +1,19 @@
 package com.transfar.server.business.server.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.transfar.common.dto.BaseResponsePackage;
-import com.transfar.common.dto.ServerPackage;
-import com.transfar.server.business.server.core.PackageConstructor;
-import com.transfar.server.business.server.service.IServerService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+import com.transfar.common.dto.BaseResponsePackage;
+import com.transfar.common.dto.ServerPackage;
+import com.transfar.server.business.server.core.PackageConstructor;
+import com.transfar.server.business.server.service.IServerService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * <p>
@@ -25,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/server")
 @Api(tags = "服务器信息")
-@Slf4j
 public class ServerController {
 
     /**
