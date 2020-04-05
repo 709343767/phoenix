64220e873d9b9e2419f7bed52c595cafcb916b86
pifeng <709343767@qq.com>
2020-04-05 16:33:26 +0800

    新建返回结果类

---
+++ monitoring-common/src/main/java/com/transfar/common/domain/Result.java
@@ -0,0 +1,42 @@
+package com.transfar.common.domain;
+
+import com.transfar.common.abs.SuperBean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 返回结果
+ * </p>
+ *
+ * @author 皮锋
+ * @custom.date 2020年4月5日 下午1:27:44
+ */
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+public class Result extends SuperBean {
+
+	/**
+	 * 是否成功
+	 */
+	private boolean isSuccess;
+
+	/**
+	 * 结果信息
+	 */
+	private String msg;
+
+}
