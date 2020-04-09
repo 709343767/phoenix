98c0387f386c80ad31ec8f161329ad5572fdb090
pifeng <709343767@qq.com>
2020-04-09 11:06:45 +0800

    修改项目名

---
+++ monitoring-agent/shutdown_prod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#先通过管理端口关闭进程
+curl -X POST http://localhost:16000/monitoring-agent/actuator/shutdown
+
+#再次通过名称检查进程是否被成功停止
+count=`ps -ef |grep monitoring-agent |grep -v "grep" |wc -l`
+if [ $count -gt 0 ]; then
+    if [ -f "$pid_file" ]; then
+        #如果存在进程ID文件，则读取进程ID使用信号量通知方式关闭进程
+        pid=`cat $pid_file`
+        kill -15 $pid
+    else
+        #通过名称方式查找到进程ID，使用信号量通知方式关闭进程
+        pid=`ps -ef |grep monitoring-agent |grep -v "grep"| awk '{print $2}'`
+        kill -15 $pid
+    fi
+fi
--- monitoring-server-web/startup_prod.sh
+++ monitoring-server-web/startup_prod.sh
@@ -1,1 +1,1 @@
-nohup java -jar monitoring-server-web-0.0.1-RELEASE.jar --spring.profiles.active=prod >/dev/null 2>&1 &
+nohup java -jar monitoring-server-web.jar --spring.profiles.active=prod >/dev/null 2>&1 &
