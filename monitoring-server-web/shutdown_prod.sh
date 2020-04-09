c1539427929a98c62e9190f70204b4154e937222
pifeng <709343767@qq.com>
2020-04-09 15:27:10 +0800

    格式化代码

--- monitoring-server-web/shutdown_prod.sh
+++ monitoring-server-web/shutdown_prod.sh
@@ -9,9 +9,11 @@ if [ $count -gt 0 ]; then
     #如果存在进程ID文件，则读取进程ID使用信号量通知方式关闭进程
     pid=$(cat $pid_file)
     kill -15 $pid
+    echo "killed $pid"
   else
     #通过名称方式查找到进程ID，使用信号量通知方式关闭进程
     pid=$(ps -ef | grep monitoring-server-web | grep -v "grep" | awk '{print $2}')
     kill -15 $pid
+    echo "killed $pid"
   fi
 fi
