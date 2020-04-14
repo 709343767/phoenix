e17f5a80d5adfbbb6fba128defb31a18794e8ee9
pifeng <709343767@qq.com>
2020-04-14 16:55:51 +0800

    优化代码

--- monitoring-agent/startup_prod.sh
+++ monitoring-agent/startup_prod.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 nohup java -jar monitoring-agent.jar --spring.profiles.active=prod >/dev/null 2>&1 &
+# shellcheck disable=SC2009
+pid=$(ps -ef | grep monitoring-agent.jar | grep -v "grep" | awk '{print $2}')
+echo "start succeeded,pid is $pid !"
