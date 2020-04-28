64f5a82d1cf1fb22b7032c8aab5668650e7da72c
pifeng <709343767@qq.com>
2020-04-28 15:04:17 +0800

    增加环境判断：只有生产环境才持久化所有信息池

--- monitoring-server-web/src/main/java/com/transfar/server/business/server/core/PersistPoolLifecycle.java
+++ monitoring-server-web/src/main/java/com/transfar/server/business/server/core/PersistPoolLifecycle.java
@@ -11,6 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -22,15 +23,17 @@ import java.util.Map;
  * <p>
  * 持久化所有信息池
  * </p>
+ * 如果是生成环境：<br>
  * 1.在Spring容器初始化时从文件系统中加载所有信息池内容到Spring容器；<br>
- * 2.在Spring容器销毁时持久化所有Spring容器中的信息池内容到文件系统；<br>
- * 3.定时每十分钟把所有Spring容器中的信息池内容持久化到文件系统<br>
+ * 2.定时每十分钟把所有Spring容器中的信息池内容持久化到文件系统;<br>
+ * 3.在Spring容器销毁时持久化所有Spring容器中的信息池内容到文件系统。<br>
  *
  * @author 皮锋
  * @custom.date 2020/3/25 16:38
  */
 @Component
 @Slf4j
+@Profile("prod")
 public class PersistPoolLifecycle implements InitializingBean, DisposableBean {
 
     /**
@@ -82,13 +85,90 @@ public class PersistPoolLifecycle implements InitializingBean, DisposableBean {
      * <p>
      * 在Spring容器初始化时加载资源
      * </p>
-     * 1.把持久化到文件系统的信息池内容加载到Spring容器
+     * 把持久化到文件系统的信息池内容加载到Spring容器
      *
      * @author 皮锋
      * @custom.date 2020/3/31 16:17
      */
     @Override
     public void afterPropertiesSet() {
+        // 把持久化到文件系统的信息池内容加载到Spring容器
+        this.loadPools();
+    }
+
+    /**
+     * <p>
+     * 在Spring容器销毁时处理资源
+     * </p>
+     * 1.把Spring容器中的信息池内容存入文件系统；<br>
+     * 2.把Spring容器中的信息池清空<br>
+     *
+     * @author 皮锋
+     * @custom.date 2020/3/25 16:43
+     */
+    @Override
+    public void destroy() {
+        // 把Spring容器中的信息池内容存入文件系统
+        this.storePools();
+        // 把Spring容器中的信息池清空
+        this.clear();
+    }
+
+    /**
+     * <p>
+     * 把Spring容器中的信息池内容存入文件系统
+     * </p>
+     *
+     * @author 皮锋
+     * @custom.date 2020年3月31日 下午3:40:05
+     */
+    private void storePools() {
+        try {
+            FileUtils.writeStringToFile(new File(FileNameConstants.INSTANCE_POOL), this.instancePool.toJsonString(),
+                    Charsets.UTF_8, false);
+            log.info("把Spring容器中的应用实例池内容存入文件系统成功！");
+        } catch (Exception e) {
+            log.error("把Spring容器中的应用实例池内容存入文件系统异常！");
+        }
+        try {
+            FileUtils.writeStringToFile(new File(FileNameConstants.NET_POOL), this.netPool.toJsonString(),
+                    Charsets.UTF_8, false);
+            log.info("把Spring容器中的网络信息池内容存入文件系统成功！");
+        } catch (Exception e) {
+            log.error("把Spring容器中的网络信息池内容存入文件系统异常！");
+        }
+        try {
+            FileUtils.writeStringToFile(new File(FileNameConstants.MEMORY_POOL), this.memoryPool.toJsonString(),
+                    Charsets.UTF_8, false);
+            log.info("把Spring容器中的服务器内存信息池内容存入文件系统成功！");
+        } catch (Exception e) {
+            log.error("把Spring容器中的服务器内存信息池内容存入文件系统异常！");
+        }
+        try {
+            FileUtils.writeStringToFile(new File(FileNameConstants.CPU_POOL), this.cpuPool.toJsonString(),
+                    Charsets.UTF_8, false);
+            log.info("把Spring容器中的服务器CPU信息池内容存入文件系统成功！");
+        } catch (Exception e) {
+            log.error("把Spring容器中的服务器CPU信息池内容存入文件系统异常！");
+        }
+        try {
+            FileUtils.writeStringToFile(new File(FileNameConstants.DISK_POOL), this.diskPool.toJsonString(),
+                    Charsets.UTF_8, false);
+            log.info("把Spring容器中的服务器磁盘信息池内容存入文件系统成功！");
+        } catch (Exception e) {
+            log.error("把Spring容器中的服务器磁盘信息池内容存入文件系统异常！");
+        }
+    }
+
+    /**
+     * <p>
+     * 把持久化到文件系统的信息池内容加载到Spring容器
+     * </p>
+     *
+     * @author 皮锋
+     * @custom.date 2020/4/28 14:39
+     */
+    private void loadPools() {
         try {
             String instancePoolStr = FileUtils.readFileToString(new File(FileNameConstants.INSTANCE_POOL), Charsets.UTF_8);
             if (StringUtils.isNotBlank(instancePoolStr)) {
@@ -160,70 +240,6 @@ public class PersistPoolLifecycle implements InitializingBean, DisposableBean {
             log.info("把文件系统中的服务器磁盘信息池内容加载到Spring容器成功！");
         } catch (Exception ignored) {
             log.info("把文件系统中的服务器磁盘信息池内容加载到Spring容器异常！");
-        }
-    }
-
-    /**
-     * <p>
-     * 在Spring容器销毁时处理资源
-     * </p>
-     * 1.把Spring容器中的信息池内容存入文件系统；<br>
-     * 2.把Spring容器中的信息池清空<br>
-     *
-     * @author 皮锋
-     * @custom.date 2020/3/25 16:43
-     */
-    @Override
-    public void destroy() {
-        // 把Spring容器中的信息池内容存入文件系统
-        this.storePools();
-        // 把Spring容器中的信息池清空
-        this.clear();
-    }
-
-    /**
-     * <p>
-     * 把Spring容器中的信息池内容存入文件系统
-     * </p>
-     *
-     * @author 皮锋
-     * @custom.date 2020年3月31日 下午3:40:05
-     */
-    private void storePools() {
-        try {
-            FileUtils.writeStringToFile(new File(FileNameConstants.INSTANCE_POOL), this.instancePool.toJsonString(),
-                    Charsets.UTF_8, false);
-            log.info("把Spring容器中的应用实例池内容存入文件系统成功！");
-        } catch (Exception e) {
-            log.error("把Spring容器中的应用实例池内容存入文件系统异常！");
-        }
-        try {
-            FileUtils.writeStringToFile(new File(FileNameConstants.NET_POOL), this.netPool.toJsonString(),
-                    Charsets.UTF_8, false);
-            log.info("把Spring容器中的网络信息池内容存入文件系统成功！");
-        } catch (Exception e) {
-            log.error("把Spring容器中的网络信息池内容存入文件系统异常！");
-        }
-        try {
-            FileUtils.writeStringToFile(new File(FileNameConstants.MEMORY_POOL), this.memoryPool.toJsonString(),
-                    Charsets.UTF_8, false);
-            log.info("把Spring容器中的服务器内存信息池内容存入文件系统成功！");
-        } catch (Exception e) {
-            log.error("把Spring容器中的服务器内存信息池内容存入文件系统异常！");
-        }
-        try {
-            FileUtils.writeStringToFile(new File(FileNameConstants.CPU_POOL), this.cpuPool.toJsonString(),
-                    Charsets.UTF_8, false);
-            log.info("把Spring容器中的服务器CPU信息池内容存入文件系统成功！");
-        } catch (Exception e) {
-            log.error("把Spring容器中的服务器CPU信息池内容存入文件系统异常！");
-        }
-        try {
-            FileUtils.writeStringToFile(new File(FileNameConstants.DISK_POOL), this.diskPool.toJsonString(),
-                    Charsets.UTF_8, false);
-            log.info("把Spring容器中的服务器磁盘信息池内容存入文件系统成功！");
-        } catch (Exception e) {
-            log.error("把Spring容器中的服务器磁盘信息池内容存入文件系统异常！");
         }
     }
 
