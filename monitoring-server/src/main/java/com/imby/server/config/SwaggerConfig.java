5d736f9b73bf09321ec297068423826e482e4d4f
pifeng <709343767@qq.com>
2020-05-08 12:20:30 +0800

    只有开发环境才使用Swagger

--- monitoring-server/src/main/java/com/imby/server/config/SwaggerConfig.java
+++ monitoring-server/src/main/java/com/imby/server/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.imby.server.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -22,6 +23,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @Slf4j
+@Profile("dev")
 public class SwaggerConfig {
 
     /**
