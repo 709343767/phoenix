83d297422b07ad7d56afbd413f8164204955056e
pifeng <709343767@qq.com>
2020-12-31 15:18:52 +0800

    SwaggerBootstrapUi配置权限

--- monitoring-server/src/main/java/com/gitee/pifeng/server/config/SwaggerConfig.java
+++ monitoring-server/src/main/java/com/gitee/pifeng/server/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.gitee.pifeng.server.config;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +23,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@EnableSwaggerBootstrapUI
 @Slf4j
 public class SwaggerConfig {
 
