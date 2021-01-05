038f81d1b177a916af256dfb816db58bb9a78fd1
pifeng <709343767@qq.com>
2021-01-05 09:03:15 +0800

    修改sql

--- monitoring-server/src/main/java/com/gitee/pifeng/server/constant/sql/Oracle.java
+++ monitoring-server/src/main/java/com/gitee/pifeng/server/constant/sql/Oracle.java
@@ -29,12 +29,30 @@ public class Oracle {
     /**
      * 会话列表
      */
-    public static final String SESSION_LIST = "SELECT s.SID AS SID,s.SERIAL# AS SERIAL# ,s.USERNAME AS USERNAME,s.SCHEMANAME AS SCHEMANAME,s.TYPE AS TYPE," +
-            "s.STATE AS STATE,s.LOGON_TIME AS LOGONTIME,s.MACHINE AS MACHINE,s.OSUSER AS OSUSER,s.PROGRAM AS PROGRAM," +
-            "s.EVENT AS EVENT,s.SECONDS_IN_WAIT AS WAITTIME, to_char(substr(sq.SQL_FULLTEXT,0,2000)) AS SQL " +
-            "FROM GV$SESSION s, gv$sql sq " +
-            "WHERE s.sql_address = sq.address(+) " +
-            "AND s.sql_hash_value = sq.hash_value(+)  AND s.sql_child_number = sq.child_number AND s.TYPE = 'USER' ORDER BY s.SID ASC";
+    public static final String SESSION_LIST = "SELECT " +
+            "s.SID AS SID, " +
+            "s.SERIAL# AS SERIAL# , " +
+            "s.USERNAME AS USERNAME, " +
+            "s.SCHEMANAME AS SCHEMANAME, " +
+            "s.TYPE AS TYPE, " +
+            "s.STATE AS STATE, " +
+            "s.LOGON_TIME AS LOGONTIME, " +
+            "s.MACHINE AS MACHINE, " +
+            "s.OSUSER AS OSUSER, " +
+            "s.PROGRAM AS PROGRAM, " +
+            "s.EVENT AS EVENT, " +
+            "s.SECONDS_IN_WAIT AS WAITTIME, " +
+            "to_char(substr(sq.SQL_FULLTEXT, 0, 2000)) AS SQL " +
+            "FROM " +
+            "GV$SESSION s, " +
+            "GV$sql sq " +
+            "WHERE " +
+            "s.sql_address = sq.address(+) " +
+            "AND s.sql_hash_value = sq.hash_value(+) " +
+            "AND s.sql_child_number = sq.child_number(+) " +
+            "AND s.TYPE = 'USER' " +
+            "ORDER BY " +
+            "s.SID ASC";
 
     /**
      * 结束会话
@@ -45,24 +63,24 @@ public class Oracle {
      * 查询表空间
      */
     public static final String TABLE_SPACE_SELECT = "SELECT " +
-            "b.file_id fileId, " +
-            "b.file_name fileName, " +
-            "b.tablespace_name tablespaceName, " +
-            "b.bytes total, " +
-            "(b.bytes-sum(nvl(a.bytes, 0))) used, " +
-            "sum(nvl(a.bytes, 0)) free, " +
-            "sum(nvl(a.bytes, 0))/(b.bytes)* 100 freePer " +
+            "a.file_id fileId, " +
+            "a.file_name fileName, " +
+            "a.tablespace_name tablespaceName, " +
+            "a.bytes total, " +
+            "(a.bytes-sum(nvl(b.bytes, 0))) used, " +
+            "sum(nvl(b.bytes, 0)) FREE, " +
+            "sum(nvl(b.bytes, 0))/(a.bytes)* 100 freePer " +
             "FROM " +
-            "dba_free_space a, " +
-            "dba_data_files b " +
+            "dba_data_files a, " +
+            "dba_free_space b " +
             "WHERE " +
-            "a.file_id = b.file_id " +
+            "a.file_id = b.file_id(+) " +
             "GROUP BY " +
-            "b.tablespace_name, " +
-            "b.file_name, " +
-            "b.file_id, " +
-            "b.bytes " +
+            "a.tablespace_name, " +
+            "a.file_name, " +
+            "a.file_id, " +
+            "a.bytes " +
             "ORDER BY " +
-            "b.tablespace_name";
+            "a.tablespace_name";
 
 }
