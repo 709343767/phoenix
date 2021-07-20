0cf8e4bfca2820a9810ed90e2e83743b08995b54
流玄青夏 <709343767@qq.com>
2021-07-20 14:24:28 +0800

    新增jvm调优参数，更好地节约服务器内存

--- doc/shell脚本/代理端/restart_agent_prod.sh
+++ doc/shell脚本/代理端/restart_agent_prod.sh
@@ -36,6 +36,6 @@ if (($count > 0)); then
 fi
 
 #启动进程
-nohup java -jar -Xms128m -Xmx128m monitoring-agent.jar --spring.profiles.active=prod >/dev/null 2>&1 &
+nohup java -jar -Xms64m -Xmx128m monitoring-agent.jar --spring.profiles.active=prod >/dev/null 2>&1 &
 pid=$(ps -ef | grep monitoring-agent.jar | grep -v "grep" | awk '{print $2}')
 echo "监控代理端进程已经启动，进程ID为：$pid"
