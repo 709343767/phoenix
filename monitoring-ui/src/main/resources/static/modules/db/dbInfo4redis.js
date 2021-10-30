34920bb195c45e15e240ed31d21788de47303733
pifeng <709343767@qq.com>
2021-10-30 16:15:27 +0800

    redis详情页面增加是否自动刷新开关

--- monitoring-ui/src/main/resources/static/modules/db/dbInfo4redis.js
+++ monitoring-ui/src/main/resources/static/modules/db/dbInfo4redis.js
@@ -1,121 +1,146 @@
 /** layuiAdmin.std-v2020.4.1 LPPL License By 皮锋 */
 ;layui.define(function (e) {
-    layui.use(['index', 'element', 'layer'], function () {
-        var $ = layui.$, admin = layui.admin, element = layui.element, layer = layui.layer;
-        // 弹出loading框
-        var loadingIndex = layer.load(1, {
-            shade: [0.1, '#fff'] //0.1透明度的白色背景
+    layui.use(['index', 'element', 'layer', 'form'], function () {
+        var $ = layui.$, admin = layui.admin, element = layui.element, layer = layui.layer, form = layui.form;
+        // 是否自动刷新
+        var redisAutoRefresh = true;
+        form.on('checkbox(redisAutoRefresh)', function (data) {
+            //是否被选中，true或者false
+            redisAutoRefresh = data.elem.checked;
         });
-        admin.req({
-            type: 'get',
-            url: layui.setter.base + 'db-info4redis/get-redis-info',
-            dataType: 'json',
-            contentType: 'application/json;charset=utf-8',
-            headers: {
-                "X-CSRF-TOKEN": tokenValue
-            },
-            data: {
-                id: id, // 数据库主键ID
-            },
-            success: function (result) {
-                var info = result.data;
-                // 定义要展示的内容
-                var server, clients, memory, persistence, stats, replication, cpu, cluster, keyspace;
-                if (!isEmpty(info)) {
-                    info = info.replace(new RegExp('\r\n\r\n', 'g'), '<br><br>')
-                        .replace(new RegExp('\n\r\n\r', 'g'), '<br><br>')
-                        .replace(new RegExp('\n\r', 'g'), '<br>')
-                        .replace(new RegExp('\r\n', 'g'), '<br>');
-                    var infos = info.split('<br><br>');
-                    for (var i = 0; i < infos.length; i++) {
-                        if (infos[i].indexOf('# Server') !== -1) {
-                            server = infos[i].replace('# Server<br>', '');
-                        }
-                        if (infos[i].indexOf('# Clients') !== -1) {
-                            clients = infos[i].replace('# Clients<br>', '');
-                        }
-                        if (infos[i].indexOf('# Memory') !== -1) {
-                            memory = infos[i].replace('# Memory<br>', '');
-                        }
-                        if (infos[i].indexOf('# Persistence') !== -1) {
-                            persistence = infos[i].replace('# Persistence<br>', '');
-                        }
-                        if (infos[i].indexOf('# Stats') !== -1) {
-                            stats = infos[i].replace('# Stats<br>', '');
-                        }
-                        if (infos[i].indexOf('# Replication') !== -1) {
-                            replication = infos[i].replace('# Replication<br>', '');
-                        }
-                        if (infos[i].indexOf('# CPU') !== -1) {
-                            cpu = infos[i].replace('# CPU<br>', '');
-                        }
-                        if (infos[i].indexOf('# Cluster') !== -1) {
-                            cluster = infos[i].replace('# Cluster<br>', '');
-                        }
-                        if (infos[i].indexOf('# Keyspace') !== -1) {
-                            keyspace = infos[i].replace('# Keyspace', '');
+
+        // 获取redis信息
+        function getRedisInfo() {
+            // 弹出loading框
+            var loadingIndex = layer.load(1, {
+                shade: [0.1, '#fff'] //0.1透明度的白色背景
+            });
+            admin.req({
+                type: 'get',
+                url: layui.setter.base + 'db-info4redis/get-redis-info',
+                dataType: 'json',
+                contentType: 'application/json;charset=utf-8',
+                headers: {
+                    "X-CSRF-TOKEN": tokenValue
+                },
+                data: {
+                    id: id, // 数据库主键ID
+                },
+                success: function (result) {
+                    var info = result.data;
+                    // 定义要展示的内容
+                    var server, clients, memory, persistence, stats, replication, cpu, cluster, keyspace;
+                    if (!isEmpty(info)) {
+                        info = info.replace(new RegExp('\r\n\r\n', 'g'), '<br><br>')
+                            .replace(new RegExp('\n\r\n\r', 'g'), '<br><br>')
+                            .replace(new RegExp('\n\r', 'g'), '<br>')
+                            .replace(new RegExp('\r\n', 'g'), '<br>');
+                        var infos = info.split('<br><br>');
+                        for (var i = 0; i < infos.length; i++) {
+                            if (infos[i].indexOf('# Server') !== -1) {
+                                server = infos[i].replace('# Server<br>', '');
+                            }
+                            if (infos[i].indexOf('# Clients') !== -1) {
+                                clients = infos[i].replace('# Clients<br>', '');
+                            }
+                            if (infos[i].indexOf('# Memory') !== -1) {
+                                memory = infos[i].replace('# Memory<br>', '');
+                            }
+                            if (infos[i].indexOf('# Persistence') !== -1) {
+                                persistence = infos[i].replace('# Persistence<br>', '');
+                            }
+                            if (infos[i].indexOf('# Stats') !== -1) {
+                                stats = infos[i].replace('# Stats<br>', '');
+                            }
+                            if (infos[i].indexOf('# Replication') !== -1) {
+                                replication = infos[i].replace('# Replication<br>', '');
+                            }
+                            if (infos[i].indexOf('# CPU') !== -1) {
+                                cpu = infos[i].replace('# CPU<br>', '');
+                            }
+                            if (infos[i].indexOf('# Cluster') !== -1) {
+                                cluster = infos[i].replace('# Cluster<br>', '');
+                            }
+                            if (infos[i].indexOf('# Keyspace') !== -1) {
+                                keyspace = infos[i].replace('# Keyspace', '');
+                            }
                         }
                     }
+                    var html = '<div class="layui-col-xs12 layui-col-sm4">' +
+                        '          <div class="layui-collapse">' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Server</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + server + '</div>' +
+                        '             </div>' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Clients</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + clients + '</div>' +
+                        '             </div>' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Cluster</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + cluster + '</div>' +
+                        '             </div>' +
+                        '          </div>' +
+                        '       </div>' +
+                        '       <div class="layui-col-xs12 layui-col-sm4">' +
+                        '          <div class="layui-collapse">' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Memory</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + memory + '</div>' +
+                        '             </div>' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">CPU</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + cpu + '</div>' +
+                        '             </div>' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Stats</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + stats + '</div>' +
+                        '             </div>' +
+                        '          </div>' +
+                        '       </div>' +
+                        '       <div class="layui-col-xs12 layui-col-sm4">' +
+                        '          <div class="layui-collapse">' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Replication</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + replication + '</div>' +
+                        '             </div>' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Persistence</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + persistence + '</div>' +
+                        '             </div>' +
+                        '             <div class="layui-colla-item">' +
+                        '                <h2 class="layui-colla-title">Keyspace</h2>' +
+                        '                <div class="layui-colla-content layui-show">' + keyspace + '</div>' +
+                        '             </div>' +
+                        '          </div>' +
+                        '       </div>';
+                    $('#redis-info').html(html);
+                    //更新全部
+                    element.render();
+                    // 关闭loading框
+                    layer.close(loadingIndex);
+                },
+                error: function () {
+                    // 关闭loading框
+                    layer.close(loadingIndex);
                 }
-                var html = '<div class="layui-col-xs12 layui-col-sm4">' +
-                    '          <div class="layui-collapse">' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Server</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + server + '</div>' +
-                    '             </div>' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Clients</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + clients + '</div>' +
-                    '             </div>' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Cluster</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + cluster + '</div>' +
-                    '             </div>' +
-                    '          </div>' +
-                    '       </div>' +
-                    '       <div class="layui-col-xs12 layui-col-sm4">' +
-                    '          <div class="layui-collapse">' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Memory</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + memory + '</div>' +
-                    '             </div>' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">CPU</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + cpu + '</div>' +
-                    '             </div>' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Stats</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + stats + '</div>' +
-                    '             </div>' +
-                    '          </div>' +
-                    '       </div>' +
-                    '       <div class="layui-col-xs12 layui-col-sm4">' +
-                    '          <div class="layui-collapse">' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Replication</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + replication + '</div>' +
-                    '             </div>' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Persistence</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + persistence + '</div>' +
-                    '             </div>' +
-                    '             <div class="layui-colla-item">' +
-                    '                <h2 class="layui-colla-title">Keyspace</h2>' +
-                    '                <div class="layui-colla-content layui-show">' + keyspace + '</div>' +
-                    '             </div>' +
-                    '          </div>' +
-                    '       </div>';
-                $('#redis-info').html(html);
-                //更新全部
-                element.render();
-                // 关闭loading框
-                layer.close(loadingIndex);
-            },
-            error: function () {
-                // 关闭loading框
-                layer.close(loadingIndex);
+            });
+        }
+
+        // 执行ajax请求
+        function execute() {
+            // 获取redis信息
+            getRedisInfo();
+        }
+
+        // 页面加载后第一次执行
+        execute();
+        // 每30秒刷新一次
+        window.setInterval(function () {
+            if (redisAutoRefresh) {
+                execute();
             }
-        });
+        }, 1000 * 30);
     });
     e('dbInfo4redis', {});
 });
