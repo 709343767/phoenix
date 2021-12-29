9c29a9db1d2e0dc30bd2f604eba951b8d85bbf8c
pifeng <709343767@qq.com>
2021-12-29 10:39:59 +0800

    让docker中能监控主机

--- doc/脚本/代理端/docker_run_agent.sh
+++ doc/脚本/代理端/docker_run_agent.sh
@@ -1,1 +1,1 @@
-docker run -itd -v /tmp:/tmp -v /liblog4phoenix:/liblog4phoenix -v /etc/localtime:/etc/localtime:ro -p 12000:12000 --name phoenix-agent phoenix/phoenix-agent /bin/bash
+docker run -itd -v /tmp:/tmp -v /liblog4phoenix:/liblog4phoenix -v /etc/localtime:/etc/localtime:ro -p 12000:12000 --pid host --net host --name phoenix-agent phoenix/phoenix-agent /bin/bash
