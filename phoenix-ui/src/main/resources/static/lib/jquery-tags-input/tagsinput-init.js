c0302ccb687ec2812438eb5667d9c18c19690278
pifeng <709343767@qq.com>
2022-04-03 22:43:16 +0800

    美化监控配置界面

---
+++ phoenix-ui/src/main/resources/static/lib/jquery-tags-input/tagsinput-init.js
@@ -0,0 +1,19 @@
+//tag input
+$(function () {
+    // 收件人邮箱
+    $('textarea[name="alarmMailEmills"]').tagsInput({
+        width: 'auto',
+        'delimiter': ';',
+        onChange: function (elem, elem_tags) {
+            $('.tag').css('background-color', '#D2D2D2');
+        }
+    });
+    // 收件人号码
+    $('textarea[name="alarmSmsPhoneNumbers"]').tagsInput({
+        width: 'auto',
+        'delimiter': ';',
+        onChange: function (elem, elem_tags) {
+            $('.tag').css('background-color', '#D2D2D2');
+        }
+    });
+});
