025aede9836eea057caa6c8ba3f2d7dad64f61b2
pifeng <709343767@qq.com>
2022-06-27 15:37:56 +0800

    1.升级oshi版本到6.2.0； 2.升级jna版本到5.12.0； 3.获取Windows下系统负载； 4.修改oshi获取交换区总量调用的方法；

--- phoenix-common/phoenix-common-core/src/main/java/com/gitee/pifeng/monitoring/common/init/InitOshi.java
+++ phoenix-common/phoenix-common-core/src/main/java/com/gitee/pifeng/monitoring/common/init/InitOshi.java
@@ -1,6 +1,7 @@
 package com.gitee.pifeng.monitoring.common.init;
 
 import oshi.SystemInfo;
+import oshi.util.GlobalConfig;
 
 /**
  * <p>
@@ -15,5 +16,21 @@ public class InitOshi {
     /**
      * 初始化oshi，并创建SystemInfo对象
      */
-    public static final SystemInfo SYSTEM_INFO = new SystemInfo();
+    public static final SystemInfo SYSTEM_INFO = initOshi();
+
+    /**
+     * <p>
+     * 初始化oshi
+     * </p>
+     *
+     * @return {@link SystemInfo}
+     * @author 皮锋
+     * @custom.date 2022/6/27 14:51
+     */
+    private static SystemInfo initOshi() {
+        // 全局配置属性
+        GlobalConfig.set(GlobalConfig.OSHI_OS_WINDOWS_LOADAVERAGE, true);
+        return new SystemInfo();
+    }
+
 }
