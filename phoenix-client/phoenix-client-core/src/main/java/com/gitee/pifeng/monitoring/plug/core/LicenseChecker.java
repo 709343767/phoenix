6dad2acf2faedac5b647063e3522e65c75bf5532
流玄青夏 <709343767@qq.com>
2025-04-25 16:24:44 +0800

    努力解决JCE cannot authenticate the provider BC的问题

--- phoenix-client/phoenix-client-core/src/main/java/com/gitee/pifeng/monitoring/plug/core/LicenseChecker.java
+++ phoenix-client/phoenix-client-core/src/main/java/com/gitee/pifeng/monitoring/plug/core/LicenseChecker.java
@@ -1,6 +1,7 @@
 package com.gitee.pifeng.monitoring.plug.core;
 
 import cn.hutool.core.io.IoUtil;
+import cn.hutool.crypto.GlobalBouncyCastleProvider;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.crypto.asymmetric.KeyType;
 import com.alibaba.fastjson.JSON;
@@ -15,6 +16,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.security.Provider;
+import java.security.Security;
 import java.time.LocalDateTime;
 
 /**
@@ -32,6 +35,26 @@ public class LicenseChecker {
      * 解密公钥
      */
     private static final String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyFQ82udUbJIjENoqvyK9PMXr1JjdCypWL+9S7H7XhWlvjivyK6y4VrkZYj6UOe5uV6f3t4mYtg+1fEEJ4WuO5Y2FYgIL+ZIlLEHiz+gaAPmpoGoFR3dfQcLvaZ6Yv2K9Q9SlSi7cazMRTBB4eq6GgB6x6bfMvMML7TfvBe0qwPSeURfatZbPWYuDNArt3wfnk3yMiJxhZLRLnYOg/C3Qs6DC9jffKejIH95bxERbRoMvwjo6wEUZqOsVZHby6PYhE4uxNm3nMpSTX8j7c2CCIXEkQJoIYfI/XEb5H9Z1+8Qvk/tlEPrmgYvqJw7b9drmfkikIvNJ2P8k0SCaqFJqhQIDAQAB";
+
+    /*
+     * 参考资料：
+     * https://blog.csdn.net/qq_36963762/article/details/122338213
+     * https://blog.csdn.net/u012586326/article/details/143574259
+     * https://www.cnblogs.com/hellxz/p/18530533
+     * https://blog.csdn.net/weixin_40816738/article/details/108164270
+     * https://github.com/chinabugotech/hutool/issues/2631
+     */
+    static {
+        if (Security.getProvider("BC") == null) {
+            // 注册 Bouncy Castle 提供程序
+            Provider provider = GlobalBouncyCastleProvider.INSTANCE.getProvider();
+            // Security.insertProviderAt(provider, 1);
+            Security.addProvider(provider);
+            // 强制关闭Bouncy Castle而使用JDK
+            // GlobalBouncyCastleProvider.setUseBouncyCastle(false);
+            log.info("注册加密提供程序：Bouncy Castle({})！", Security.getProvider("BC"));
+        }
+    }
 
     /**
      * <p>
