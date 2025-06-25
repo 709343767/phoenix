cb3d0a4db4f0deec999903aa4512b130bfaa8a19
pifeng <709343767@qq.com>
2025-06-25 12:54:43 +0800

    修改创建数据库的dockerfile

--- doc/Docker/mysql/run_container.sh
+++ doc/Docker/mysql/run_container.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # 出错立即退出
 set -e
-# 宿主机目录
+# 定义容器名称和宿主机目录
+CONTAINER_NAME="phoenix-mysql"
 HOST_DATA_DIR="/data/phoenix/mysql"
 # 检查父目录是否有写权限
 if [ ! -w "$(dirname "$HOST_DATA_DIR")" ]; then
@@ -10,21 +11,21 @@ if [ ! -w "$(dirname "$HOST_DATA_DIR")" ]; then
 fi
 # 创建目录（如果不存在）
 mkdir -p "${HOST_DATA_DIR}/data"
-#赋予读写权限
+# 赋予读写权限
 chmod -R o+rw "${HOST_DATA_DIR}"
 # 删除已存在的容器（如果有的话）
-if [ "$(docker inspect --format='{{.Id}}' phoenix-mysql 2>/dev/null)" ]; then
-  echo "Removing existing container 'phoenix-mysql'..."
-  docker stop phoenix-mysql > /dev/null 2>&1 || true
-  docker rm phoenix-mysql > /dev/null 2>&1
+if [ "$(docker inspect --format='{{.Id}}' ${CONTAINER_NAME} 2>/dev/null)" ]; then
+  echo "Removing existing container '${CONTAINER_NAME}'..."
+  docker stop ${CONTAINER_NAME} > /dev/null 2>&1 || true
+  docker rm ${CONTAINER_NAME} > /dev/null 2>&1
 fi
 # 启动容器
-echo "Starting phoenix-mysql container..."
+echo "Starting ${CONTAINER_NAME} container..."
 docker run -itd \
   -p 3306:3306 \
   --restart unless-stopped \
   -v "${HOST_DATA_DIR}/data:/var/lib/mysql" \
-  --name phoenix-mysql \
+  --name ${CONTAINER_NAME} \
   crpi-4iaxdbbs1euymfiu.cn-shanghai.personal.cr.aliyuncs.com/pifeng_phoenix/mysql:1.2.6.RELEASE-CR3
 # 启动容器成功
-echo "Container 'phoenix-mysql' started successfully."
+echo "Container '${CONTAINER_NAME}' started successfully."
