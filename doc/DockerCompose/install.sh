66e801535a36b9531418de1083831edf331e0b44
pifeng <709343767@qq.com>
2025-06-26 16:38:53 +0800

    新增dockercompose文件

---
+++ doc/DockerCompose/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 定义 docker-compose.yml 文件的URL
+COMPOSE_FILE_URL="https://gitee.com/monitoring-platform/phoenix/raw/master/doc/DockerCompose/docker-compose.1.2.6.RELEASE-CR3.yml"
+
+# 定义本地存储路径
+LOCAL_COMPOSE_FILE="./docker-compose.yml"
+
+# 下载 docker-compose.yml 文件
+echo "Downloading docker-compose.yml..."
+curl -o $LOCAL_COMPOSE_FILE -L $COMPOSE_FILE_URL
+if [ $? -ne 0 ]; then
+  echo "Failed to download docker-compose.yml"
+  exit 1
+fi
+
+# 检查是否已安装 docker-compose
+if ! command -v docker-compose &> /dev/null
+then
+    echo "docker-compose could not be found, please install it."
+    exit 1
+fi
+
+# 使用 docker-compose 启动服务
+echo "Starting services with docker-compose..."
+docker-compose -f $LOCAL_COMPOSE_FILE up -d
+
+# 检查 docker-compose 命令是否成功
+if [ $? -eq 0 ]; then
+  echo "Services started successfully."
+else
+  echo "Failed to start services."
+  exit 1
+fi
