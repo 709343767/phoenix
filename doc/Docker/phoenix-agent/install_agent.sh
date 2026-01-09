0c6e32f942c822fd52bb167ea6bb9d1a5d0c6c2e
pifeng <709343767@qq.com>
2026-01-10 04:56:08 +0800

    新增 install_agent.sh 脚本

--- doc/Docker/phoenix-agent/install_agent.sh
+++ doc/Docker/phoenix-agent/install_agent.sh
@@ -2,7 +2,7 @@
 # 出错立即退出
 set -e
 
-echo "Starting to execute remote scripts..."
+echo "Starting to execute remote script..."
 
 # 定义一个远程脚本的 URL
 RUN_CONTAINER_PHOENIX_AGENT_URL="https://gitee.com/monitoring-platform/phoenix/raw/master/doc/Docker/phoenix-agent/run_container.1.2.6.RELEASE-CR5.sh"
